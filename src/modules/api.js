975724d6910dfbfb4adf95f07bcf6afb952b7ced
youknowHRT <1021300769@qq.com>
2020-11-23 17:35:48 +0800

    pre2

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,7 @@
 // let host = 'http://mock.hunger-valley.com/cnode/api/v1'
 // let host = ' https://52.197.183.123/api/v1'
-// let host = 'https://www.easy-mock.com/mock/5fb8db183f90e10d966465a6/api/v1'
-let host= 'http://rap2api.taobao.org/app/mock/271351/api/v1'
+let host = 'https://www.easy-mock.com/mock/5fb8db183f90e10d966465a6/api/v1'
+// let host= 'http://rap2api.taobao.org/app/mock/271351/api/v1'
 // https://api.prodless.com/avatar.png
 // http://www.splitbrain.org/_static/monsterid/monsterid.php
 
